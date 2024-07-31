cafc02866fa6daae0c154004c39a2c4277c7185e
kimHyeonJu <kimhyesom1202@gmail.com>
2024-07-31 17:42:37 +0900

    [구성] basic reset 설정
    - global.css
    - reset.css
    - miniReset.css
    - main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-import React from 'react';
-
 function App() {
   return (
-    <div className="app">
-      <h1>React 웹 앱</h1>
-    </div>
+    <div className="app"></div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
-import '@/styles/globals.css';
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+// > css
+import '@/styles/globals.css';
+import '@/styles/miniReset.css';
+import '@/styles/reset.css';
 // > components
 import App from '@/App.jsx';
 

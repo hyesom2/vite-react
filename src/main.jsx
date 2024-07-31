a9ba1acff7627d8980e06fbae79bcfbbefb7fc49
kimHyeonJu <kimhyesom1202@gmail.com>
2024-07-31 17:07:37 +0900

    [튜토리얼] Vite custom template (7)
    - (@) 절대 경로 구성
    - 개발 서버 구성

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
+import '@/styles/globals.css';
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 // > components
-import App from '../App.jsx';
+import App from '@/App.jsx';
 
 const rootNode = document.getElementById('react-app');
 createRoot(rootNode).render(

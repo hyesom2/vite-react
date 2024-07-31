b5cc7e485726f10994b2274230eef178beded03e
kimHyeonJu <kimhyesom1202@gmail.com>
2024-07-31 15:37:11 +0900

    [튜토리얼] Vite custom template (4)
    - @vitejs/plugin-react 설치
    - Vite 구성 파일 생성 후, 플러그인 연결

---
+++ App.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>React 웹 앱</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-function App() {
-  return (
-    <div className="app">
-      <h1>React 웹 앱</h1>
-    </div>
-  );
-}
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+// > components
+import App from '../App.jsx';
 
 const rootNode = document.getElementById('react-app');
-ReactDOM.createRoot(rootNode).render(
-  <React.StrictMode>
+createRoot(rootNode).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
